178d5dcbc2eb44d25756be3fa3d8c34b199cfdea
Thomas Aunvik <contact@thaun.dev>
2025-08-28 15:09:46 +0200

    Add Missing CA Extension (PR #5)

--- gencerts.sh
+++ gencerts.sh
@@ -12,7 +12,7 @@ touch certs/index.txt
 export SUBJECT_ALT_NAME=localhost
 export SUBJECT_ALT_IP=127.0.0.1
 
-[ -e certs/root.crt -a -e certs/root.key ] || openssl req -config openssl.cnf -x509 -nodes -newkey rsa -keyout certs/root.key -out certs/root.crt -subj '/C=NO/O=root'
+[ -e certs/root.crt -a -e certs/root.key ] || openssl req -config openssl.cnf -extensions v3_ca -x509 -nodes -newkey rsa -keyout certs/root.key -out certs/root.crt -subj '/C=NO/O=root'
 openssl req -config openssl.cnf -days 3650 -nodes -newkey rsa -keyout certs/scproxy.key -out certs/scproxy.csr -extensions v3_req -subj "/C=NO/CN=${SUBJECT_ALT_NAME}"
 openssl ca -config openssl.cnf -batch -rand_serial -out certs/scproxy.crt -notext -extensions v3_req -infiles certs/scproxy.csr
 
